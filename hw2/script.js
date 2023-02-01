845a1bd5e84fa921a205b0eae4a5d5d2fd3eae2b
Moza1K <115919089+Moza1K@users.noreply.github.com>
2023-02-01 19:46:29 +0200

    Add files via upload

---
+++ hw2/script.js
@@ -0,0 +1,17 @@
+let userAge = 19;
+let userName;
+let userSurname;
+
+let newUser;
+let shoppingList;
+const isUserOnline = true;
+let salaryWorker;
+let mauseX;
+let mauseY;
+
+let greeting;
+
+greeting = `Hello World`;
+console.log(greeting)
+
+console.log(`userAge`,userAge)
